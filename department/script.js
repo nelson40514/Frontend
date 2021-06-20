821491ab13f40f098a2a62a8590ed01a35957321
NelsonSu <33085066+nelson40514@users.noreply.github.com>
2021-06-20 22:48:25 +0800

    可以修改

--- department/script.js
+++ department/script.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
+    flag = 0
     $('.modify').click(function() {
-        console.log('click')
-        $(this).parent().siblings().each(function() {
-            let data = $(this).html();
-            $(this).before(`<input>${data}</input>`);
-            $(this).remove();
-        })
+        if (flag == 0) {
+            $(this).parent().siblings().each(function() {
+                let data = $(this).html()
+                $(this).html(`<input value="${data}"></input>`)
+            })
+            flag = 1
+        } else {
+            $(this).parent().siblings().each(function() {
+                let data = $(this).children().first().val()
+                $(this).html(data)
+            })
+            flag = 0
+        }
+
     })
     $('.confirm').click(function() {
 
