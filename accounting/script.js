3ffd72115e2684dc48152ae003c29e70956296e6
NelsonSu <33085066+nelson40514@users.noreply.github.com>
2021-06-20 23:27:29 +0800

    ++

---
+++ accounting/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    flag = 0
+    $('.modify').click(modify)
+    $('.confirm').click(function() {
+            $(this).parent().parent().before('<tr><td><button class="modify">修改</button></td></tr>')
+            $(this).parent().siblings().each(function() {
+                let data = $(this).children().first().val()
+                $(this).parent().prev().append(`<td>${data}</td>`)
+            })
+            $(this).parent().parent().prev().children().first().children().first().click(modify)
+        })
+        /*Page - Level Demo Scripts - Tables - Use for reference*/
+    $('#dataTables-example').DataTable({
+        responsive: true
+    });
+})
+
+let modify = function() {
+    if (flag == 0) {
+        $(this).html('儲存')
+        $(this).parent().siblings().each(function() {
+            let data = $(this).html()
+            $(this).html(`<input value="${data}"></input>`)
+        })
+        flag = 1
+    } else {
+        $(this).html('修改')
+        $(this).parent().siblings().each(function() {
+            let data = $(this).children().first().val()
+            $(this).html(data)
+        })
+        flag = 0
+    }
+}
