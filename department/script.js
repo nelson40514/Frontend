f26ace4f9bda754780873654d76fd68fa1d843be
NelsonSu <33085066+nelson40514@users.noreply.github.com>
2021-06-20 23:15:35 +0800

    好的

--- department/script.js
+++ department/script.js
@@ -2,13 +2,17 @@ $(document).ready(function() {
     flag = 0
     $('.modify').click(modify)
     $('.confirm').click(function() {
-        $(this).parent().parent().before('<tr><td><button class="modify">修改</button></td></tr>')
-        $(this).parent().siblings().each(function() {
-            let data = $(this).children().first().val()
-            $(this).parent().prev().append(`<td>${data}</td>`)
+            $(this).parent().parent().before('<tr><td><button class="modify">修改</button></td></tr>')
+            $(this).parent().siblings().each(function() {
+                let data = $(this).children().first().val()
+                $(this).parent().prev().append(`<td>${data}</td>`)
+            })
+            $(this).parent().parent().prev().children().first().children().first().click(modify)
         })
-        $(this).parent().parent().prev().children().first().children().first().click(modify)
-    })
+        /*Page - Level Demo Scripts - Tables - Use for reference*/
+    $('#dataTables-example').DataTable({
+        responsive: true
+    });
 })
 
 let modify = function() {
